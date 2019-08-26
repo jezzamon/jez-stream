ef91d0be93c9504cbd85edf916ff81deda05b338
jezzamondev <jezzamondev@gmail.com>
2019-08-26 00:27:35 -0400

    feat/rest-Set up action creators for REST

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
   renderError = ({ error, touched }) => {
@@ -27,6 +29,7 @@ class StreamCreate extends Component {
 
   onSubmit = formValues => {
     console.log(formValues);
+    this.props.createStream(formValues);
   };
 
   render() {
@@ -66,7 +69,12 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate,
 })(StreamCreate);
+
+export default connect(
+  null,
+  { createStream }
+)(formWrapped);
