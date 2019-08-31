192b2de0c62645c9a5e7c99a4f30b53fc3dbf782
jezzamondev <jezzamondev@gmail.com>
2019-08-31 00:09:54 -0400

    get 1 item stream

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -4,16 +4,30 @@ import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
   componentDidMount() {
-    
+    this.props.fetchStream(this.props.match.params.id);
   }
+
   render() {
-    return <div>Stream show</div>;
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    const { title, description } = this.props.stream;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
   }
 }
 
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  };
+};
 export default connect(
-  null,
+  mapStateToProps,
   { fetchStream }
 )(StreamShow);
-
-
